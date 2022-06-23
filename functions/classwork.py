7280ce97e14ad57dc17d2c0fab4324b34231336b
Zonder9162 <zonder9162@gmail.com>
2022-06-23 12:01:04 +0600

    built-in

--- functions/classwork.py
+++ functions/classwork.py
@@ -86,10 +86,157 @@
 # action = input('Введите один из символов для действия "+","-","/","*": ')
 # print(get_data(action))
 
-def cooking_icecream(name, size, *args):
-    print(f'Ваше {name} мороженное {size} размера')
-    if args:
-        print('Ваши топпинги: ')
-        for i in args:
-            print(i)
-cooking_icecream('шоколадное', 'большого', 'арахис', 'ммеллад')
+# def cooking_icecream(name, size, *args):
+#     print(f'Ваше {name} мороженное {size} размера')
+#     if args:
+#         print('Ваши топпинги: ')
+#         for i in args:
+#             print(i)
+# cooking_icecream('шоколадное', 'большого', 'арахис', 'ммеллад')
+
+# def foo():
+#     var = 'переменная foo'
+  
+#     def bar():
+#         var = 'переменная bar'
+ 
+#     bar()
+# foo()
+# print('переменная в foo: ', var)
+
+# number = '1321'
+# if int(number[-1]) < int(number[-2]) and int(number[-2])<int(number[-3]):
+#     if int(number[-3]) < int(number[-4]):
+#         print('YES')
+#     else:
+#         print('NO')
+# else:
+#         print('NO')
+
+# number = '4326'
+# if int(number[-1]) < int(number[-2]) and int(number[-2])<int(number[-3]) and int(number[-3]) < int(number[-4]):
+#         print('YES')
+# else:
+#         print('NO')
+
+# def foo():
+#     global var
+#     var = 'переменная foo'
+  
+#     def bar():
+#         global var
+#         print('переменная в foo: ', var)
+#         var = 'переменная bar'
+        
+#     bar()
+# foo()
+# print('переменная в foo: ', var)
+
+# x = 'Я глобальная переменная!'
+# def my_func():
+#     global x
+#     print(x)
+#     x = 'Я могу изменяться'
+# my_func()
+# print(x)
+
+# num = 3
+# def mul():
+#     global num
+#     num **=2
+# mul()
+# mul()
+# mul()
+# print(num)
+
+# balance = 0
+# def get_salary(amount):
+#     global balance
+#     balance += amount
+# def get_balance():
+#     global balance
+#     print(f'У вас на счету {balance} сом')
+# def pay_bills(amount, log_name):
+#     global balance
+#     print(f"Вы заплатили {amount} сом за {log_name}")
+#     balance -= amount
+# def get_balance():
+#     global balance
+#     print(f'У вас на счету {balance} сом')
+
+# get_salary(1000)
+# get_balance()
+# pay_bills(400, 'кабельное тв')
+# get_balance()
+
+# result = 0
+# def pow_first(x,y):
+#     global result
+#     result = x**y
+# def pow_second(z):
+#     global result
+#     result %= z
+# pow_first(2, 3)
+# pow_second(3)
+# print(result)
+
+# a = {'Мурат': 24, 'Эржан': 21, 'Чынгыз': 24, 'Алтынай': 17, 'Асема': 16}
+# def age_control():
+#     global a
+#     for k, v in a.items():
+#         if v >= 17:
+#             print(f'{k}, Вы можете войти в клуб')
+#         else:
+#             print(f'{k}, извините, Вы не проходите по age-control')
+# age_control()
+
+# a = ['pipi', 'papa', 'mama']
+# def registr():
+#     global a
+#     b = []
+#     for i in a:
+#         b.append(i.title())
+#     print(b)
+# registr()
+
+# glas = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
+# sogl = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ']
+# num_glas = 0
+# num_sogl = 0
+# ostolnye = 0
+
+# def count_symbols(string):
+#     global glas, sogl, num_glas, num_sogl, ostolnye
+#     for i in string.lower():
+#         if i in glas:
+#             num_glas += 1
+#         elif i in sogl:
+#             num_sogl += 1
+#         else:
+#             ostolnye += 1
+#     print(f'Количество гласных: {num_glas}, согласных: {num_sogl}, остальных символов: {ostolnye}')
+
+# count_symbols('Мурат супер!')
+
+# a = [1, 3, 4, 6, 8, 6, 8, 9, 0, 3]
+# def menshe_semi(list_):
+#     for i in list_:
+#         if i > 7:
+#             list_.remove(i)
+#     print(list_)
+# menshe_semi(a)
+
+main_matryoshka = 5
+def size():
+    global main_matryoshka
+    medium_matryoshka = 4
+    sum_matryoshka = main_matryoshka +  medium_matryoshka
+    def full_size():
+        nonlocal sum_matryoshka
+        little_matryoshka = 3
+        full_sum_matryoshka = sum_matryoshka + little_matryoshka 
+        print(full_sum_matryoshka)
+    full_size()
+size()
+
+
